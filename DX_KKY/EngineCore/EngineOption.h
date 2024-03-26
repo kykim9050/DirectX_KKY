85a074efd717e08cf8d6d1b2c64cc7c42dd38cab
kykim9050 <kykim9050@naver.com>
2024-03-26 15:18:52 +0900

    김경윤 : EngineBase, EngineCore - 업데이트

--- DX_KKY/EngineCore/EngineOption.h
+++ DX_KKY/EngineCore/EngineOption.h
@@ -6,6 +6,10 @@
 #include <iostream>
 #include <format>
 
+// 설명 : 엔진의 옵션 데이터를 저장하는 용도의 구조체
+// 내부에 값형 이외의 것을 넣으면 너죽고 나죽자.
+// 가상함수 포인터 std::string 등등이 들어가면 안되는데.
+// std::string이 안들어가는건 너무 불편하죠?
 struct FEngineOption : public UEngineSerializeObject
 {
 public:
@@ -15,10 +19,12 @@ public:
 	void Serialize(UEngineSerializer& _Ser) override
 	{
 		{
+			// c++ 20에서 문자열 최신 버전이 나왔어요.
+
 			std::string DebugOptionText;
 
-			DebugOptionText += std::format("WindowScale : {}, {}\n", WindowScale.iX(), WindowScale.iY());
-			DebugOptionText += std::format("IsDebug : {}\n", IsDebug);
+			DebugOptionText += std::format("WindowScale : [{}], [{}]\n", WindowScale.iX(), WindowScale.iY());
+			DebugOptionText += std::format("IsDebug : [{}]\n", IsDebug);
 
 			_Ser.WriteText(DebugOptionText);
 		}
@@ -27,6 +33,8 @@ public:
 	void DeSerialize(UEngineSerializer& _Ser) override
 	{
 		std::string OptionText = _Ser.ToString();
+
+		std::vector<std::string> Values = UEngineString::StringCutting(OptionText, "[", "]");
 	}
 
 };
