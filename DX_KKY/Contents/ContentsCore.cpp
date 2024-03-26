7f941b5cc788cf7737a96b119ef5a73f627e186a
kky <kykim9050@naver.com>
2024-03-26 16:11:18 +0900

    김경윤 : Contents 추가 , EngineBase, EngineCore - 업데이트

---
+++ DX_KKY/Contents/ContentsCore.cpp
@@ -0,0 +1,16 @@
+﻿#include "ContentsCore.h"
+
+UContentsCore::UContentsCore()
+{
+}
+
+UContentsCore::~UContentsCore()
+{
+}
+
+void UContentsCore::Initialize()
+{
+	// GEngine->CreateLevel();
+	int a = 0;
+}
+
