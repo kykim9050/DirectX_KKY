1bd64e855f4858efea33db40bd37db5277c2407f
kykim9050 <kykim9050@naver.com>
2024-03-26 15:09:13 +0900

    김경윤 : EngineCore - 업데이트

---
+++ DX_KKY/EngineCore/EngineCore.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// Ό³Έν :
+class UEngineCore
+{
+public:
+	// constrcuter destructer
+	UEngineCore();
+	~UEngineCore();
+
+	// delete Function
+	UEngineCore(const UEngineCore& _Other) = delete;
+	UEngineCore(UEngineCore&& _Other) noexcept = delete;
+	UEngineCore& operator=(const UEngineCore& _Other) = delete;
+	UEngineCore& operator=(UEngineCore&& _Other) noexcept = delete;
+
+protected:
+
+private:
+
+};
+
