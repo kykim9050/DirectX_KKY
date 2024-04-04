69e970407a64eb3d672a7e72b3ed5f3b1410d1d0
kky <kykim9050@naver.com>
2024-04-04 15:55:56 +0900

    김경윤 : Texture 출력 업데이트

--- DX_KKY/Contents/ContentsCore.cpp
+++ DX_KKY/Contents/ContentsCore.cpp
@@ -12,6 +12,17 @@ UContentsCore::~UContentsCore()
 
 void UContentsCore::Initialize()
 {
+	{
+		UEngineDirectory Dir;
+		Dir.MoveToSearchChild("ContentsResources");
+		Dir.Move("Image");
+		std::vector<UEngineFile> Files = Dir.GetAllFile({ ".png" }, true);
+		for (UEngineFile& File : Files)
+		{
+			UEngineTexture::Load(File.GetFullPath());
+		}
+	}
+
 	GEngine->CreateLevel<APlayGameMode>("PlayLevel");
 	GEngine->ChangeLevel("PlayLevel");
 }
