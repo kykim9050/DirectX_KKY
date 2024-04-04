2a3f429a7f4cc0956b8d25f2c224089687adacda
kykim9050 <kykim9050@naver.com>
2024-04-04 15:55:56 +0900

    김경윤 : Texture 출력 업데이트

--- DX_KKY/Contents/Player.cpp
+++ DX_KKY/Contents/Player.cpp
@@ -24,7 +24,12 @@ void APlayer::BeginPlay()
 {
 	Super::BeginPlay();
 
-	SetActorScale3D(FVector(100.0f, 100.0f, 100.0f));
+	SetActorScale3D(FVector(300.0f, 300.0f, 100.0f));
+
+	// 안해주면 터져야한다.
+	Renderer->Resources->SettingConstantBuffer("OutPutColor", Color);
+	// 내부에서 샘플러도 같이 찾을
+	Renderer->Resources->SettingTexture("Image", "CharIdle0.png", "POINT");
 }
 
 void APlayer::Tick(float _DeltaTime)
@@ -53,6 +58,37 @@ void APlayer::Tick(float _DeltaTime)
 	{
 		AddActorLocation(FVector::Down * _DeltaTime * Speed);
 	}
+
+	if (true == UEngineInput::IsPress(VK_NUMPAD1))
+	{
+		Color.X += _DeltaTime;
+	}
+
+	if (true == UEngineInput::IsPress(VK_NUMPAD2))
+	{
+		Color.X -= _DeltaTime;
+	}
+
+	if (true == UEngineInput::IsPress(VK_NUMPAD4))
+	{
+		Color.Y += _DeltaTime;
+	}
+
+	if (true == UEngineInput::IsPress(VK_NUMPAD5))
+	{
+		Color.Y -= _DeltaTime;
+	}
+
+	if (true == UEngineInput::IsPress(VK_NUMPAD7))
+	{
+		Color.Z += _DeltaTime;
+	}
+
+	if (true == UEngineInput::IsPress(VK_NUMPAD8))
+	{
+		Color.Z -= _DeltaTime;
+	}
+
 
 
 	// Renderer->SetConstanctBuffer("Ftransform", GetTransform());
