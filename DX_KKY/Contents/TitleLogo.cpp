88edfb9cf6520ac30927aabc39aa9b05eb06af40
kykim9050 <kykim9050@naver.com>
2024-04-05 19:04:26 +0900

    김경윤 : 엔진 업데이트

--- DX_KKY/Contents/TitleLogo.cpp
+++ DX_KKY/Contents/TitleLogo.cpp
@@ -16,10 +16,11 @@ void ATitleLogo::BeginPlay()
 {
 	Super::BeginPlay();
 
-	SetActorScale3D(FVector(700.0f, 300.0f, 100.0f));
+	SetActorScale3D(FVector(1280.0f, 720.0f, 100.0f));
 
-	//Renderer->SetSamplering(ETextureSampling::LINEAR);
+	
 	Renderer->SetSprite("Title");
+	Renderer->SetSamplering(ETextureSampling::LINEAR);
 	Renderer->CreateAnimation("TitlePlay", "Title", 0.08f);
 	Renderer->ChangeAnimation("TitlePlay");
 
