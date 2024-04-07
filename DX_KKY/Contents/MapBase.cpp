cbffebc6dc0606a4ea95f0a41591c93647c2f6dc
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-04-07 18:11:48 +0900

    김경윤 : Contents - MapBase 클래스 추가

---
+++ DX_KKY/Contents/MapBase.cpp
@@ -0,0 +1,22 @@
+#include "PreCompile.h"
+#include "MapBase.h"
+
+AMapBase::AMapBase()
+{
+}
+
+AMapBase::~AMapBase()
+{
+}
+
+void AMapBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AMapBase::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+}
+
+
---
+++ DX_KKY/Contents/MapBase.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <EngineCore/Actor.h>
+
+// Ό³Έν :
+class AMapBase : public AActor
+{
+	GENERATED_BODY(AActor)
+
+public:
+	// constrcuter destructer
+	AMapBase();
+	~AMapBase();
+
+	// delete Function
+	AMapBase(const AMapBase& _Other) = delete;
+	AMapBase(AMapBase&& _Other) noexcept = delete;
+	AMapBase& operator=(const AMapBase& _Other) = delete;
+	AMapBase& operator=(AMapBase&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+
+private:
+
+};
+
