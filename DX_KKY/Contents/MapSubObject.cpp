6cba73f809442d1208dec12f1f4401312ca2463c
kykim9050 <kykim9050@naver.com>
2024-04-08 18:16:58 +0900

    김경윤 : Contents - TutorialMode에 MapSubObject 추가

--- DX_KKY/Contents/MapSubObject.cpp
+++ DX_KKY/Contents/MapSubObject.cpp
@@ -12,9 +12,6 @@ AMapSubObject::~AMapSubObject()
 void AMapSubObject::BeginPlay()
 {
 	Super::BeginPlay();
-
-	SetMapFile("tutorial_room_back_layer_0001.png");
-	SetMapScale(FVector{ 1280.0f, 720.0f, 0.0f });
 }
 
 void AMapSubObject::Tick(float _DeltaTime)
