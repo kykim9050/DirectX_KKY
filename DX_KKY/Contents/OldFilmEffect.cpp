f53d4e4fb1fc973f06d7fca027d32ea14cb9cef3
kykim9050 <kykim9050@naver.com>
2024-04-08 13:50:58 +0900

    김경윤 : OldFilmEffect 클래스 추가

---
+++ DX_KKY/Contents/OldFilmEffect.cpp
@@ -0,0 +1,20 @@
+﻿#include "PreCompile.h"
+#include "OldFilmEffect.h"
+
+AOldFilmEffect::AOldFilmEffect()
+{
+}
+
+AOldFilmEffect::~AOldFilmEffect()
+{
+}
+
+void AOldFilmEffect::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AOldFilmEffect::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+}
---
+++ DX_KKY/Contents/OldFilmEffect.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <EngineCore/Actor.h>
+
+// Ό³Έν :
+class AOldFilmEffect : public AActor
+{
+	GENERATED_BODY(AActor)
+public:
+	// constrcuter destructer
+	AOldFilmEffect();
+	~AOldFilmEffect();
+
+	// delete Function
+	AOldFilmEffect(const AOldFilmEffect& _Other) = delete;
+	AOldFilmEffect(AOldFilmEffect&& _Other) noexcept = delete;
+	AOldFilmEffect& operator=(const AOldFilmEffect& _Other) = delete;
+	AOldFilmEffect& operator=(AOldFilmEffect&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private:
+
+};
+
