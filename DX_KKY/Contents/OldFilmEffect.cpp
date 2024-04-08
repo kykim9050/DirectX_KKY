bf56b6cfea2d0e7ce5f39e8a6093e633f9c113f2
kky <kykim9050@naver.com>
2024-04-08 18:33:45 +0900

    김경윤 : Contents - OldFilm효과 적용

--- DX_KKY/Contents/OldFilmEffect.cpp
+++ DX_KKY/Contents/OldFilmEffect.cpp
@@ -1,8 +1,13 @@
 ﻿#include "PreCompile.h"
+#include <EngineCore/SpriteRenderer.h>
+
 #include "OldFilmEffect.h"
+
+
 
 AOldFilmEffect::AOldFilmEffect()
 {
+	Renderer = CreateDefaultSubObject<USpriteRenderer>("OldFilmEFRenderer");
 }
 
 AOldFilmEffect::~AOldFilmEffect()
@@ -12,9 +17,20 @@ AOldFilmEffect::~AOldFilmEffect()
 void AOldFilmEffect::BeginPlay()
 {
 	Super::BeginPlay();
+
+	CreateOFEFAnimation();
+
+	Renderer->ChangeAnimation("OldFilmAni");
+
+	SetActorScale3D(FVector{1280.0f, 720.0f});
 }
 
 void AOldFilmEffect::Tick(float _DeltaTime)
 {
 	Super::Tick(_DeltaTime);
+}
+
+void AOldFilmEffect::CreateOFEFAnimation()
+{
+	Renderer->CreateAnimation("OldFilmAni", "OldFilmEffect", 0.05f);
 }
