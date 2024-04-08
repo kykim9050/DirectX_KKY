ebef6063f502d5d0fe5a1d0e4dfb2f8d5fefea3f
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-04-08 21:29:35 +0900

    김경윤 : Contents - WorldGameMode 클래스 추가

---
+++ DX_KKY/Contents/WorldGameMode.cpp
@@ -0,0 +1,21 @@
+#include "PreCompile.h"
+#include "WorldGameMode.h"
+
+
+AWorldGameMode::AWorldGameMode()
+{
+}
+
+AWorldGameMode::~AWorldGameMode()
+{
+}
+
+void AWorldGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AWorldGameMode::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+}
