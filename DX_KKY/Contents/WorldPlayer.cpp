22b87920858568958e14cb8d67dae2bcb600372e
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-04-09 07:51:04 +0900

    김경윤 : Contents - WorldPlayer 클래스 추가

---
+++ DX_KKY/Contents/WorldPlayer.cpp
@@ -0,0 +1,21 @@
+#include "PreCompile.h"
+
+#include "WorldPlayer.h"
+
+AWorldPlayer::AWorldPlayer()
+{
+}
+
+AWorldPlayer::~AWorldPlayer()
+{
+}
+
+void AWorldPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AWorldPlayer::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+}
