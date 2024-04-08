bbfa5feb1d1d93b71f08c078ac417b48417e740e
kky <kykim9050@naver.com>
2024-04-08 17:55:42 +0900

    김경윤 : Contents - Tutorial 맵 적용

---
+++ DX_KKY/Contents/MapSubObject.cpp
@@ -0,0 +1,23 @@
+﻿#include "PreCompile.h"
+#include "MapSubObject.h"
+
+AMapSubObject::AMapSubObject()
+{
+}
+
+AMapSubObject::~AMapSubObject()
+{
+}
+
+void AMapSubObject::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetMapFile("tutorial_room_back_layer_0001.png");
+	SetMapScale(FVector{ 1280.0f, 720.0f, 0.0f });
+}
+
+void AMapSubObject::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+}
--- DX_KKY/Contents/TutorialMode.cpp
+++ DX_KKY/Contents/TutorialMode.cpp
@@ -4,6 +4,7 @@
 #include "TutorialMode.h"
 #include "Player.h"
 #include "TutorialMap.h"
+#include "MapSubObject.h"
 
 ATutorialMode::ATutorialMode()
 {
@@ -20,9 +21,17 @@ void ATutorialMode::BeginPlay()
 	std::shared_ptr<UCamera> Camera = GetWorld()->GetMainCamera();
 	Camera->SetActorLocation(FVector(0.0f, 0.0f, -100.0f));
 
-	GetWorld()->SpawnActor<APlayer>("Player");
-	GetWorld()->SpawnActor<ATutorialMap>("TutorialMap");
+	std::shared_ptr<AActor> Player = GetWorld()->SpawnActor<APlayer>("Player");
+	std::shared_ptr<AActor>	TutorialMap = GetWorld()->SpawnActor<ATutorialMap>("TutorialMap");
+	std::shared_ptr<AActor> BackScreen = GetWorld()->SpawnActor<AMapSubObject>("BackScreen");
+
+
+	Player->AddActorLocation(FVector{ 0.0f, 0.0f, 0.0f });
+	TutorialMap->AddActorLocation(FVector{ 0.0f, 0.0f, 100.0f });
+	BackScreen->AddActorLocation(FVector{ 0.0f, 0.0f, 200.0f });
+
 }
+
 
 void ATutorialMode::Tick(float _DeltaTime)
 {
