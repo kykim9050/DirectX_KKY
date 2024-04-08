e68bf5070bc0b7f766e374c91e011b56862a6fdc
kky <kykim9050@naver.com>
2024-04-08 17:41:43 +0900

    김경윤 : Contents - TutorialMap 추가

--- DX_KKY/Contents/TutorialMap.cpp
+++ DX_KKY/Contents/TutorialMap.cpp
@@ -14,6 +14,8 @@ void ATutorialMap::BeginPlay()
 	Super::BeginPlay();
 
 	SetMapFile("tutorial_object.png");
+	SetMapScale(FVector{6200.0f, 810.0f, 0.0f});
+
 }
 
 void ATutorialMap::Tick(float _DeltaTime)
