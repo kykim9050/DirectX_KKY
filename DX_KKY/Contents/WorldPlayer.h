1ce08356cf71895d8d27a448fa064ebfde725de9
kykim9050 <kykim9050@naver.com>
2024-04-09 07:51:04 +0900

    김경윤 : Contents - WorldPlayer 클래스 추가

---
+++ DX_KKY/Contents/WorldPlayer.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <EngineCore/Actor.h>
+
+// Ό³Έν :
+class USpriteRenderer;
+class AWorldPlayer : public AActor
+{
+	GENERATED_BODY(UTickObject)
+
+public:
+	// constrcuter destructer
+	AWorldPlayer();
+	~AWorldPlayer();
+
+	// delete Function
+	AWorldPlayer(const AWorldPlayer& _Other) = delete;
+	AWorldPlayer(AWorldPlayer&& _Other) noexcept = delete;
+	AWorldPlayer& operator=(const AWorldPlayer& _Other) = delete;
+	AWorldPlayer& operator=(AWorldPlayer&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private:
+	USpriteRenderer* Renderer;
+};
+
