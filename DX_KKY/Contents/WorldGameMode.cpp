c9753489d6308acd8842dab5b475a8bde8ec239e
kykim9050 <kykim9050@naver.com>
2024-04-09 13:51:19 +0900

    김경윤 : Contents - WorldGameMode에 OldFilm효과 적용

--- DX_KKY/Contents/WorldGameMode.cpp
+++ DX_KKY/Contents/WorldGameMode.cpp
@@ -1,10 +1,13 @@
 #include "PreCompile.h"
+#include <EngineCore/Camera.h>
 
 #include "ContentsEnumClass.h"
 #include "WorldGameMode.h"
 #include "WorldMap.h"
 #include "MapSubObject.h"
 #include "WorldPlayer.h"
+#include "OldFilmEffect.h"
+
 
 AWorldGameMode::AWorldGameMode()
 {
@@ -18,9 +21,14 @@ void AWorldGameMode::BeginPlay()
 {
 	Super::BeginPlay();
 
+	std::shared_ptr<UCamera> Camera = GetWorld()->GetMainCamera();
+	Camera->SetActorLocation(FVector(0.0f, 0.0f, -100.0f));
+
+	std::shared_ptr<AActor> OldFilm = GetWorld()->SpawnActor<AOldFilmEffect>("OldFilmEffect", static_cast<int>(EActorType::FilmEffect));
 	std::shared_ptr<AMapSubObject> MapLayer = GetWorld()->SpawnActor<AMapSubObject>("MapLayer", static_cast<int>(EActorType::BackGroundSubStaticObject));
 	std::shared_ptr<AActor> WPlayer = GetWorld()->SpawnActor<AWorldPlayer>("WorldPlayer", static_cast<int>(EActorType::Player));
 	std::shared_ptr<AActor>	WorldMap = GetWorld()->SpawnActor<AWorldMap>("WorldMap", static_cast<int>(EActorType::Map));
+
 
 	std::shared_ptr<AWorldMap> WMap = std::dynamic_pointer_cast<AWorldMap>(WorldMap);
 	
@@ -33,6 +41,7 @@ void AWorldGameMode::BeginPlay()
 	MapLayer->SetMapSubObject("WorldMap_Layer.png");
 	MapLayer->SetMapSubObjectScale(WMap->GetMapScale());
 
+	OldFilm->AddActorLocation(FVector{ 0.0f, 0.0f, 0.0f });
 	MapLayer->AddActorLocation(FVector{ 0.0f, 0.0f, 50.0f });
 	WPlayer->AddActorLocation(FVector{ 0.0f, 0.0f, 100.0f });
 	WorldMap->AddActorLocation(FVector{ 0.0f, 0.0f, 200.0f });
