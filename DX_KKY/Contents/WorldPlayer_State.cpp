1592f0cdb4ffff2cfdff9ec2b9db1a1d371cc3cc
kky <kykim9050@naver.com>
2024-04-09 18:20:05 +0900

    김경윤 : WorldPlayer - 키보드 입력별 case 추가

--- DX_KKY/Contents/WorldPlayer_State.cpp
+++ DX_KKY/Contents/WorldPlayer_State.cpp
@@ -5,46 +5,80 @@
 
 void AWorldPlayer::StateInit()
 {
-	State.CreateState("RightDownIdle");
-	State.CreateState("RightIdle");
-	State.CreateState("UpIdle");
-	State.CreateState("DownIdle");
-	State.CreateState("LeftIdle");
+	{
+		State.CreateState("UpIdle");
+		State.CreateState("RightUpIdle");
+		State.CreateState("RightIdle");
+		State.CreateState("RightDownIdle");
+		State.CreateState("DownIdle");
+		State.CreateState("LeftDownIdle");
+		State.CreateState("LeftIdle");
+		State.CreateState("LeftUpIdle");
 
-	State.SetUpdateFunction("RightDownIdle", std::bind(&AWorldPlayer::RightDownIdle, this, std::placeholders::_1));
-	State.SetUpdateFunction("RightIdle", std::bind(&AWorldPlayer::RightIdle, this, std::placeholders::_1));
-	State.SetUpdateFunction("UpIdle", std::bind(&AWorldPlayer::UpIdle, this, std::placeholders::_1));
-	State.SetUpdateFunction("DownIdle", std::bind(&AWorldPlayer::DownIdle, this, std::placeholders::_1));
-	State.SetUpdateFunction("LeftIdle", std::bind(&AWorldPlayer::LeftIdle, this, std::placeholders::_1));
+		State.SetUpdateFunction("UpIdle", std::bind(&AWorldPlayer::UpIdle, this, std::placeholders::_1));
+		State.SetUpdateFunction("RightUpIdle", std::bind(&AWorldPlayer::RightUpIdle, this, std::placeholders::_1));
+		State.SetUpdateFunction("RightIdle", std::bind(&AWorldPlayer::RightIdle, this, std::placeholders::_1));
+		State.SetUpdateFunction("RightDownIdle", std::bind(&AWorldPlayer::RightDownIdle, this, std::placeholders::_1));
+		State.SetUpdateFunction("DownIdle", std::bind(&AWorldPlayer::DownIdle, this, std::placeholders::_1));
+		State.SetUpdateFunction("LeftDownIdle", std::bind(&AWorldPlayer::LeftDownIdle, this, std::placeholders::_1));
+		State.SetUpdateFunction("LeftIdle", std::bind(&AWorldPlayer::LeftIdle, this, std::placeholders::_1));
+		State.SetUpdateFunction("LeftUpIdle", std::bind(&AWorldPlayer::LeftUpIdle, this, std::placeholders::_1));
 
-	State.SetStartFunction("RightDownIdle", [=]
-		{
-			Renderer->ChangeAnimation("WPlayer_DiagonalDownIdle");
-		}
-	);
-	State.SetStartFunction("RightIdle", [=]
-		{
-			Renderer->ChangeAnimation("WPlayer_StraightIdle");
-			Renderer->SetDir(EEngineDir::Right);
-		}
-	);
-	State.SetStartFunction("UpIdle", [=]
-		{
-			Renderer->ChangeAnimation("WPlayer_UpIdle");
-		}
-	);
-	State.SetStartFunction("DownIdle", [=]
-		{
-			Renderer->ChangeAnimation("WPlayer_DownIdle");
-		}
-	);
-	State.SetStartFunction("LeftIdle", [=]
-		{
-			Renderer->ChangeAnimation("WPlayer_StraightIdle");
-			Renderer->SetDir(EEngineDir::Left);
-		}
-	);
 
+		State.SetStartFunction("UpIdle", [=]
+			{
+				Renderer->ChangeAnimation("WPlayer_UpIdle");
+			}
+		);
+
+		State.SetStartFunction("RightUpIdle", [=]
+			{
+				Renderer->ChangeAnimation("WPlayer_DiagonalUpIdle");
+				Renderer->SetDir(EEngineDir::Right);
+			}
+		);
+
+		State.SetStartFunction("RightIdle", [=]
+			{
+				Renderer->ChangeAnimation("WPlayer_StraightIdle");
+				Renderer->SetDir(EEngineDir::Right);
+			}
+		);
+
+		State.SetStartFunction("RightDownIdle", [=]
+			{
+				Renderer->ChangeAnimation("WPlayer_DiagonalDownIdle");
+				Renderer->SetDir(EEngineDir::Right);
+			}
+		);
+		
+		State.SetStartFunction("DownIdle", [=]
+			{
+				Renderer->ChangeAnimation("WPlayer_DownIdle");
+			}
+		);
+
+		State.SetStartFunction("LeftDownIdle", [=]
+			{
+				Renderer->ChangeAnimation("WPlayer_DiagonalDownIdle");
+				Renderer->SetDir(EEngineDir::Left);
+			}
+		);
+
+		State.SetStartFunction("LeftIdle", [=]
+			{
+				Renderer->ChangeAnimation("WPlayer_StraightIdle");
+				Renderer->SetDir(EEngineDir::Left);
+			}
+		);
+
+		State.SetStartFunction("LeftUpIdle", [=]
+			{
+				Renderer->ChangeAnimation("WPlayer_DiagonalUpIdle");
+				Renderer->SetDir(EEngineDir::Left);
+			}
+		);
+	}
 
 	Renderer->SetAutoSize(1.0f, true);
 	State.ChangeState("RightDownIdle");
@@ -62,53 +96,164 @@ void AWorldPlayer::StateInit()
 
 void AWorldPlayer::UpIdle(float _DeltaTime)
 {
-	KeyCheck(_DeltaTime);
+	DirCheck();
+}
+
+void AWorldPlayer::RightUpIdle(float _DeltaTime)
+{
+	DirCheck();
 }
 
 void AWorldPlayer::RightIdle(float _DeltaTime)
 {
-	KeyCheck(_DeltaTime);
+	DirCheck();
 }
 
 void AWorldPlayer::RightDownIdle(float _DeltaTime)
 {
-	KeyCheck(_DeltaTime);
+	DirCheck();
 }
 
 void AWorldPlayer::DownIdle(float _DeltaTime)
 {
-	KeyCheck(_DeltaTime);
+	DirCheck();
+}
+
+void AWorldPlayer::LeftDownIdle(float _DeltaTime)
+{
+	DirCheck();
 }
 
 void AWorldPlayer::LeftIdle(float _DeltaTime)
 {
-	KeyCheck(_DeltaTime);
+	DirCheck();
+}
+
+void AWorldPlayer::LeftUpIdle(float _DeltaTime)
+{
+	DirCheck();
 }
 
 
-void AWorldPlayer::KeyCheck(float _DeltaTime)
+//void AWorldPlayer::KeyCheck(float _DeltaTime)
+//{
+//	if(true == IsPress(VK_DOWN))
+//	{
+//		AddActorLocation(FVector::Down * _DeltaTime * GetSpeed());
+//		//State.ChangeState("DownIdle");
+//
+//	}
+//
+//	if (true == IsPress(VK_UP))
+//	{
+//		AddActorLocation(FVector::Up * _DeltaTime * GetSpeed());
+//		//State.ChangeState("UpIdle");
+//	}
+//
+//	if (true == IsPress(VK_RIGHT))
+//	{
+//		AddActorLocation(FVector::Right * _DeltaTime * GetSpeed());
+//		//State.ChangeState("RightIdle");
+//	}
+//
+//	if (true == IsPress(VK_LEFT))
+//	{
+//		AddActorLocation(FVector::Left * _DeltaTime * GetSpeed());
+//		//State.ChangeState("LeftIdle");
+//	}
+//}
+
+void AWorldPlayer::DirCheck()
 {
-	if(true == IsPress(VK_DOWN))
+	if (true == IsAnykeyFree())
 	{
-		AddActorLocation(FVector::Down * _DeltaTime * GetSpeed());
-		State.ChangeState("DownIdle");
+		switch (Dir)
+		{
+		case EWorldPlayerDir::Up:
+		{
+			State.ChangeState("UpIdle");
+			break;
+		}
+		case EWorldPlayerDir::RightUp:
+		{
+			State.ChangeState("RightUpIdle");
+			break;
+		}
+		case EWorldPlayerDir::Right:
+		{
+			State.ChangeState("RightIdle");
+			break;
+		}
+		case EWorldPlayerDir::RightDown:
+		{
+			State.ChangeState("RightDownIdle");
+			break;
+		}
+		case EWorldPlayerDir::Down:
+		{
+			State.ChangeState("DownIdle");
+			break;
+		}
+		case EWorldPlayerDir::LeftDown:
+		{
+			State.ChangeState("LeftDownIdle");
+			break;
+		}
+		case EWorldPlayerDir::Left:
+		{
+			State.ChangeState("LeftIdle");
+			break;
+		}
+		case EWorldPlayerDir::LeftUp:
+		{
+			State.ChangeState("LeftUpIdle");
+			break;
+		}
+		default:
+			break;
+		}
+
+		return;
 	}
 
-	if (true == IsPress(VK_UP))
+	if (IsPress(VK_UP) && IsFree(VK_RIGHT))
 	{
-		AddActorLocation(FVector::Up * _DeltaTime * GetSpeed());
-		State.ChangeState("UpIdle");
+		Dir = EWorldPlayerDir::Up;
 	}
 
-	if (true == IsPress(VK_RIGHT))
+	if (IsPress(VK_UP) && IsPress(VK_RIGHT))
 	{
-		AddActorLocation(FVector::Right * _DeltaTime * GetSpeed());
-		State.ChangeState("RightIdle");
+		Dir = EWorldPlayerDir::RightUp;
 	}
 
-	if (true == IsPress(VK_LEFT))
+	if (IsPress(VK_RIGHT) && IsFree(VK_DOWN))
 	{
-		AddActorLocation(FVector::Left * _DeltaTime * GetSpeed());
-		State.ChangeState("LeftIdle");
+		Dir = EWorldPlayerDir::Right;
 	}
+
+	if (IsPress(VK_RIGHT) && IsPress(VK_DOWN))
+	{
+		Dir = EWorldPlayerDir::RightDown;
+	}
+
+	if (IsPress(VK_DOWN) && IsFree(VK_LEFT))
+	{
+		Dir = EWorldPlayerDir::Down;
+	}
+
+	if (IsPress(VK_DOWN) && IsPress(VK_LEFT))
+	{
+		Dir = EWorldPlayerDir::LeftDown;
+	}
+
+	if (IsPress(VK_LEFT) && IsFree(VK_UP))
+	{
+		Dir = EWorldPlayerDir::Left;
+	}
+
+	if (IsPress(VK_LEFT) && IsPress(VK_UP))
+	{
+		Dir = EWorldPlayerDir::LeftUp;
+	}
+
 }
