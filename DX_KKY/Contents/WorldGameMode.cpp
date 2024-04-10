33bcb55b6cae6a831e1a4fa5682694e0be83c547
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-04-10 14:52:18 +0900

    김경윤 : WorldGameMode - 코드 수정

--- DX_KKY/Contents/WorldGameMode.cpp
+++ DX_KKY/Contents/WorldGameMode.cpp
@@ -27,11 +27,11 @@ void AWorldGameMode::BeginPlay()
 	std::shared_ptr<AActor> OldFilm = GetWorld()->SpawnActor<AOldFilmEffect>("OldFilmEffect", static_cast<int>(EActorType::FilmEffect));
 	std::shared_ptr<AMapSubObject> MapLayer = GetWorld()->SpawnActor<AMapSubObject>("MapLayer", static_cast<int>(EActorType::BackGroundSubStaticObject));
 	std::shared_ptr<AActor> WPlayer = GetWorld()->SpawnActor<AWorldPlayer>("WorldPlayer", static_cast<int>(EActorType::Player));
-	std::shared_ptr<AActor>	WorldMap = GetWorld()->SpawnActor<AWorldMap>("WorldMap", static_cast<int>(EActorType::Map));
+	std::shared_ptr<AActor>	WorldMap = GetWorld()->SpawnActor<AMapBase>("WorldMap", static_cast<int>(EActorType::Map));
 	std::shared_ptr<AActor> WorldCollisionMap = GetWorld()->SpawnActor<AMapBase>("WorldCollisionMap", static_cast<int>(EActorType::Map));
 
 
-	std::shared_ptr<AWorldMap> WMap = std::dynamic_pointer_cast<AWorldMap>(WorldMap);
+	std::shared_ptr<AMapBase> WMap = std::dynamic_pointer_cast<AMapBase>(WorldMap);
 	std::shared_ptr<AMapBase> WCMap = std::dynamic_pointer_cast<AMapBase>(WorldCollisionMap);
 
 #ifdef _DEBUG
@@ -48,18 +48,22 @@ void AWorldGameMode::BeginPlay()
 	}
 #endif // _DEBUG
 
-	MapLayer->SetMapSubObject("WorldMap_Layer.png");
-	MapLayer->SetMapSubObjectScale(WMap->GetMapScale());
+	WMap->SetMapFile("WorldMap.png");
+	WMap->SetAutoScale();
 
 	WCMap->SetMapFile("WorldMap_PixelCheck.png");
 	WCMap->SetAutoScale();
 
+	MapLayer->SetMapSubObject("WorldMap_Layer.png");
+	MapLayer->SetMapSubObjectScale(WMap->GetMapScale());
+
+	
 
 	OldFilm->AddActorLocation(FVector{ 0.0f, 0.0f, 0.0f });
 	MapLayer->AddActorLocation(FVector{ 0.0f, 0.0f, 50.0f });
 	WPlayer->AddActorLocation(FVector{ 0.0f, 0.0f, 100.0f });
 	WorldMap->AddActorLocation(FVector{ 0.0f, 0.0f, 200.0f });
-	WCMap->AddActorLocation(FVector{ 0.0f, 0.0f, 300.0f });
+	WorldCollisionMap->AddActorLocation(FVector{ 0.0f, 0.0f, 300.0f });
 }
 
 void AWorldGameMode::Tick(float _DeltaTime)
