ec64ebdb63192974bbd39eeddb748962b374cbef
kky <kykim9050@naver.com>
2024-04-11 17:11:05 +0900

    김경윤 : TestGameMode 코드 수정

--- DX_KKY/Contents/TestGameMode.cpp
+++ DX_KKY/Contents/TestGameMode.cpp
@@ -22,52 +22,36 @@ ATestGameMode::~ATestGameMode()
 void ATestGameMode::BeginPlay()
 {
 	Super::BeginPlay();
-	std::shared_ptr<AActor> OldFilm = GetWorld()->SpawnActor<AOldFilmEffect>("OldFilmEffect", static_cast<int>(EActorType::FilmEffect));
-	std::shared_ptr<AMapSubObject> MapLayer = GetWorld()->SpawnActor<AMapSubObject>("MapLayer", static_cast<int>(EActorType::BackGroundSubStaticObject));
-	std::shared_ptr<AActor> WPlayer = GetWorld()->SpawnActor<AWorldPlayer>("WorldPlayer", static_cast<int>(EActorType::Player));
-	std::shared_ptr<AActor>	WorldMap = GetWorld()->SpawnActor<AMapBase>("WorldMap", static_cast<int>(EActorType::Map));
-	std::shared_ptr<AActor> WorldCollisionMap = GetWorld()->SpawnActor<AMapBase>("WorldCollisionMap", static_cast<int>(EActorType::Map));
-
-
-	std::shared_ptr<AMapBase> WMap = std::dynamic_pointer_cast<AMapBase>(WorldMap);
-	std::shared_ptr<AMapBase> WCMap = std::dynamic_pointer_cast<AMapBase>(WorldCollisionMap);
-
-#ifdef _DEBUG
-	if (nullptr == WMap)
-	{
-		MsgBoxAssert("올바르지 못한 업캐스팅 형식입니다.");
-		return;
-	}
-
-	if (nullptr == WCMap)
-	{
-		MsgBoxAssert("올바르지 못한 업캐스팅 형식입니다.");
-		return;
-	}
-#endif // _DEBUG
-
-	WMap->SetMapFile("WorldMap.png");
-	WMap->SetAutoScale();
-
-	WCMap->SetMapFile("WorldMap_PixelCheck.png");
-	WCMap->SetAutoScale();
-
-	MapLayer->SetMapSubObject("WorldMap_Layer.png");
-	MapLayer->SetMapSubObjectScale(WMap->GetMapScale());
-
-	// 콜리젼 맵을 가져와서 ContentsValue 클래스에 정보 저장
-	UContentsValue::ColMapTexture = UEngineTexture::FindRes(WCMap->GetName());
-	float4 ColMapScale = UContentsValue::ColMapTexture->GetScale();
 
 	std::shared_ptr<UCamera> Camera = GetWorld()->GetMainCamera();
-	Camera->SetActorLocation(UContentsValue::WorldMapCameraInitValue);
+	std::shared_ptr<AOldFilmEffect> OldFilm = GetWorld()->SpawnActor<AOldFilmEffect>("OldFilmEffect", static_cast<int>(EActorType::FilmEffect));
+	std::shared_ptr<AWorldPlayer> WPlayer = GetWorld()->SpawnActor<AWorldPlayer>("WorldPlayer", static_cast<int>(EActorType::Player));
+	std::shared_ptr<AMapBase> MapLayer = GetWorld()->SpawnActor<AMapBase>("MapLayer", static_cast<int>(EActorType::BackGroundSubStaticObject));
+	std::shared_ptr<AMapBase> WorldMap = GetWorld()->SpawnActor<AMapBase>("WorldMap", static_cast<int>(EActorType::Map));
+	std::shared_ptr<AMapBase> WorldCollisionMap = GetWorld()->SpawnActor<AMapBase>("WorldCollisionMap", static_cast<int>(EActorType::Map));
 
+	MapLayer->SetMapFile("WorldMap_Layer.png");
+	WorldMap->SetMapFile("WorldMap.png");
+	WorldCollisionMap->SetMapFile("WorldMap_PixelCheck.png");
+	MapLayer->SetAutoScale();
+	WorldMap->SetAutoScale();
+	WorldCollisionMap->SetAutoScale();
+
+	// 콜리젼 맵을 가져와서 ContentsValue 클래스에 정보 저장
+	UContentsValue::ColMapTexture = UEngineTexture::FindRes(WorldCollisionMap->GetName());
+	float4 ColMapScale = UContentsValue::ColMapTexture->GetScale();
+
+	// OldFilm, WPlayer는 오더링 고정
+	MapLayer->SetOrdering(ERenderingOrder::FrontLayer);
+	WorldMap->SetOrdering(ERenderingOrder::BackLayer);
+	WorldCollisionMap->SetOrdering(ERenderingOrder::CollisionLayer);
+
+	Camera->SetActorLocation(UContentsValue::WorldMapCameraInitValue);
 	OldFilm->AddActorLocation(FVector{ UContentsValue::WorldMapPlayerXInitValue, UContentsValue::WorldMapPlayerYInitValue, 0.0f });
 	WPlayer->AddActorLocation(FVector{ UContentsValue::WorldMapPlayerXInitValue, UContentsValue::WorldMapPlayerYInitValue, 100.0f });
 	MapLayer->AddActorLocation(FVector{ ColMapScale.hX(), -ColMapScale.hY(), 50.0f });
-	WMap->AddActorLocation(FVector{ ColMapScale.hX(), -ColMapScale.hY(), 200.0f });
-	WCMap->AddActorLocation(FVector{ ColMapScale.hX(), -ColMapScale.hY(), 300.0f });
-
+	WorldMap->AddActorLocation(FVector{ ColMapScale.hX(), -ColMapScale.hY(), 200.0f });
+	WorldCollisionMap->AddActorLocation(FVector{ ColMapScale.hX(), -ColMapScale.hY(), 300.0f });
 
 }
 
