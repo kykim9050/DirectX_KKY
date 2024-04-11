5eff9fa7b25bc12f82d59f50162af66909455e4a
kykim9050 <kykim9050@naver.com>
2024-04-11 12:06:21 +0900

    김경윤 : WorldDust 랜더링 출력 구현

--- DX_KKY/Contents/WorldDust.cpp
+++ DX_KKY/Contents/WorldDust.cpp
@@ -16,14 +16,17 @@ void AWorldDust::BeginPlay()
 {
 	Super::BeginPlay();
 
-	Renderer->CreateAnimation("WorldDust_1", "WorldDustA", 0.1f, false);
-	Renderer->CreateAnimation("WorldDust_2", "WorldDustB", 0.1f, false);
-	Renderer->CreateAnimation("WorldDust_3", "WorldDustC", 0.1f, false);
-	Renderer->CreateAnimation("WorldDust_4", "WorldDustD", 0.1f, false);
-	Renderer->CreateAnimation("WorldDust_5", "WorldDustE", 0.1f, false);
-	Renderer->CreateAnimation("WorldDust_6", "WorldDustF", 0.1f, false);
+	//Renderer->CreateAnimation("WorldDust_1", "WorldDustA", 0.1f, false);
+	Renderer->CreateAnimation("WorldDust_1", "WorldDustA", 0.05f);
+	Renderer->CreateAnimation("WorldDust_2", "WorldDustB", 0.05f, false);
+	Renderer->CreateAnimation("WorldDust_3", "WorldDustC", 0.05f, false);
+	Renderer->CreateAnimation("WorldDust_4", "WorldDustD", 0.05f, false);
+	Renderer->CreateAnimation("WorldDust_5", "WorldDustE", 0.05f, false);
+	Renderer->CreateAnimation("WorldDust_6", "WorldDustF", 0.05f, false);
 
 	Renderer->ChangeAnimation("WorldDust_1");
+	Renderer->SetAutoSize(1.0f, true);
+	Renderer->SetOrder(10);
 }
 
 void AWorldDust::Tick(float _DeltaTime)
