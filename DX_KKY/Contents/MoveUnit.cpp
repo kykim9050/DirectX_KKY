6753c391f5e85dcee813908ec499c300a5df5eb1
kky <kykim9050@naver.com>
2024-04-12 10:01:45 +0900

    김경윤 : MoveUnit 클래스 추가

---
+++ DX_KKY/Contents/MoveUnit.cpp
@@ -0,0 +1,22 @@
+﻿#include "PreCompile.h"
+
+#include "MoveUnit.h"
+
+UMoveUnit::UMoveUnit()
+{
+}
+
+UMoveUnit::~UMoveUnit()
+{
+}
+
+void UMoveUnit::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UMoveUnit::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+}
+
