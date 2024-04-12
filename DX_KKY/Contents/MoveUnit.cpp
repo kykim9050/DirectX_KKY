36cfebc2be782923374f88d5380f19d159d7e916
kky <kykim9050@naver.com>
2024-04-12 10:12:39 +0900

    김경윤 : MoveUnit - 이동관련 함수 추가

--- DX_KKY/Contents/MoveUnit.cpp
+++ DX_KKY/Contents/MoveUnit.cpp
@@ -20,3 +20,23 @@ void UMoveUnit::Tick(float _DeltaTime)
 	Super::Tick(_DeltaTime);
 }
 
+void UMoveUnit::ResultMovementUpdate(float _DeltaTime)
+{
+	//CalHorizonVelocityVector(_DeltaTime);
+	//CalGravityVelocityVector(_DeltaTime);
+	//CalJumpVelocityVector(_DeltaTime);
+	CalMovementVector(_DeltaTime);
+	ApplyMovement(_DeltaTime);
+}
+
+void UMoveUnit::CalMovementVector(float _DeltaTime)
+{
+	TotalMovementVec = FVector::Zero;
+	TotalMovementVec = TotalMovementVec/* + SpeedVec + GravityVec + JumpVec*/;
+}
+
+void UMoveUnit::ApplyMovement(float _DeltaTime)
+{
+	AddActorLocation(TotalMovementVec * _DeltaTime);
+
+}
