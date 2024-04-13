41fe2bdf1feed70bb4db2583992389b629615ea6
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-04-13 22:46:55 +0900

    김경윤 : Player - DuckIdle 기능 추가

--- DX_KKY/Contents/Player.cpp
+++ DX_KKY/Contents/Player.cpp
@@ -48,48 +48,68 @@ void APlayer::Tick(float _DeltaTime)
 
 void APlayer::CreatePlayerAnimation()
 {
-	Renderer->CreateAnimation("Player_Idle", "Cuphead_Idle", 0.04f);
-	Renderer->CreateAnimation("Player_Run", "CupHead_Run", 0.033f);
-	Renderer->CreateAnimation("Player_Jump", "CupHead_Jump", 0.034f);
-	Renderer->CreateAnimation("Player_Parry", "CupHead_Parry", 0.033f, false);
+	{
+		Renderer->CreateAnimation("Player_Idle", "Cuphead_Idle", 0.04f);
+		Renderer->CreateAnimation("Player_Run", "CupHead_Run", 0.033f);
+		Renderer->CreateAnimation("Player_Jump", "CupHead_Jump", 0.034f);
+		Renderer->CreateAnimation("Player_Parry", "CupHead_Parry", 0.033f, false);
 
-	Renderer->CreateAnimation("Player_Aim_Straight", "CupHead_Aim_Straight", 0.04f);
-	Renderer->CreateAnimation("Player_Aim_Up", "CupHead_Aim_Up", 0.04f);
-	Renderer->CreateAnimation("Player_Aim_Down", "CupHead_Aim_Down", 0.04f);
-	Renderer->CreateAnimation("Player_Aim_DiagonalUp", "CupHead_Aim_DiagonalUp", 0.04f);
-	Renderer->CreateAnimation("Player_Aim_DiagonalDown", "CupHead_Aim_DiagonalDown", 0.04f);
+		Renderer->CreateAnimation("Player_Aim_Straight", "CupHead_Aim_Straight", 0.04f);
+		Renderer->CreateAnimation("Player_Aim_Up", "CupHead_Aim_Up", 0.04f);
+		Renderer->CreateAnimation("Player_Aim_Down", "CupHead_Aim_Down", 0.04f);
+		Renderer->CreateAnimation("Player_Aim_DiagonalUp", "CupHead_Aim_DiagonalUp", 0.04f);
+		Renderer->CreateAnimation("Player_Aim_DiagonalDown", "CupHead_Aim_DiagonalDown", 0.04f);
 
-	Renderer->CreateAnimation("Player_Duck", "CupHead_Duck", 0.033f, false);
-	Renderer->CreateAnimation("Player_DuckIdle", "CupHead_DuckIdle", 0.04f);
+		Renderer->CreateAnimation("Player_Duck", "CupHead_Duck", 0.033f, false);
+		Renderer->CreateAnimation("Player_DuckIdle", "CupHead_DuckIdle", 0.04f);
 
-	Renderer->CreateAnimation("Player_Dash", "CupHead_Dash", 0.034f, false);
-	Renderer->CreateAnimation("Player_Dash_Air", "CupHead_Dash_Air", 0.034f, false);
+		Renderer->CreateAnimation("Player_Dash", "CupHead_Dash", 0.034f, false);
+		Renderer->CreateAnimation("Player_Dash_Air", "CupHead_Dash_Air", 0.034f, false);
 
-	Renderer->CreateAnimation("Player_Run_Shoot_Straight", "CupHead_Run_Shoot_Straight", 0.033f);
-	Renderer->CreateAnimation("Player_Run_Shoot_DiagonalUp", "CupHead_Run_Shoot_DiagonalUp", 0.033f);
+		Renderer->CreateAnimation("Player_Run_Shoot_Straight", "CupHead_Run_Shoot_Straight", 0.033f);
+		Renderer->CreateAnimation("Player_Run_Shoot_DiagonalUp", "CupHead_Run_Shoot_DiagonalUp", 0.033f);
 
-	Renderer->CreateAnimation("Player_Shoot_Straight", "CupHead_Shoot_Straight", 0.033f, false);
-	Renderer->CreateAnimation("Player_Shoot_Up", "CupHead_Shoot_Up", 0.033f, false);
-	Renderer->CreateAnimation("Player_Shoot_Down", "CupHead_Shoot_Down", 0.033f, false);
-	Renderer->CreateAnimation("Player_Shoot_DiagonalUp", "CupHead_Shoot_DiagonalUp", 0.033f, false);
-	Renderer->CreateAnimation("Player_Shoot_DiagonalDown", "CupHead_Shoot_DiagonalDown", 0.033f, false);
-	Renderer->CreateAnimation("Player_Duck_Shoot", "CupHead_Duck_Shoot", 0.033f, false);
+		Renderer->CreateAnimation("Player_Shoot_Straight", "CupHead_Shoot_Straight", 0.033f, false);
+		Renderer->CreateAnimation("Player_Shoot_Up", "CupHead_Shoot_Up", 0.033f, false);
+		Renderer->CreateAnimation("Player_Shoot_Down", "CupHead_Shoot_Down", 0.033f, false);
+		Renderer->CreateAnimation("Player_Shoot_DiagonalUp", "CupHead_Shoot_DiagonalUp", 0.033f, false);
+		Renderer->CreateAnimation("Player_Shoot_DiagonalDown", "CupHead_Shoot_DiagonalDown", 0.033f, false);
+		Renderer->CreateAnimation("Player_Duck_Shoot", "CupHead_Duck_Shoot", 0.033f, false);
 
-	Renderer->CreateAnimation("Player_SSAir_Straight", "CupHead_SSAir_Straight", 0.0416f, false);
-	Renderer->CreateAnimation("Player_SSAir_Up", "CupHead_SSAir_Up", 0.0416f, false);
-	Renderer->CreateAnimation("Player_SSAir_Down", "CupHead_SSAir_Down", 0.0416f, false);
-	Renderer->CreateAnimation("Player_SSAir_DiagonalUp", "CupHead_SSAir_DiagonalUp", 0.0416f, false);
-	Renderer->CreateAnimation("Player_SSAir_DiagonalDown", "CupHead_SSAir_DiagonalDown", 0.0416f, false);
+		Renderer->CreateAnimation("Player_SSAir_Straight", "CupHead_SSAir_Straight", 0.0416f, false);
+		Renderer->CreateAnimation("Player_SSAir_Up", "CupHead_SSAir_Up", 0.0416f, false);
+		Renderer->CreateAnimation("Player_SSAir_Down", "CupHead_SSAir_Down", 0.0416f, false);
+		Renderer->CreateAnimation("Player_SSAir_DiagonalUp", "CupHead_SSAir_DiagonalUp", 0.0416f, false);
+		Renderer->CreateAnimation("Player_SSAir_DiagonalDown", "CupHead_SSAir_DiagonalDown", 0.0416f, false);
 
-	Renderer->CreateAnimation("Player_SSGround_Straight", "CupHead_SSGround_Straight", 0.0416f, false);
-	Renderer->CreateAnimation("Player_SSGround_Up", "CupHead_SSGround_Up", 0.0416f, false);
-	Renderer->CreateAnimation("Player_SSGround_Down", "CupHead_SSGround_Down", 0.0416f, false);
-	Renderer->CreateAnimation("Player_SSGround_DiagonalUp", "CupHead_SSGround_DiagonalUp", 0.0416f, false);
-	Renderer->CreateAnimation("Player_SSGround_DiagonalDown", "CupHead_SSGround_DiagonalDown", 0.0416f, false);
+		Renderer->CreateAnimation("Player_SSGround_Straight", "CupHead_SSGround_Straight", 0.0416f, false);
+		Renderer->CreateAnimation("Player_SSGround_Up", "CupHead_SSGround_Up", 0.0416f, false);
+		Renderer->CreateAnimation("Player_SSGround_Down", "CupHead_SSGround_Down", 0.0416f, false);
+		Renderer->CreateAnimation("Player_SSGround_DiagonalUp", "CupHead_SSGround_DiagonalUp", 0.0416f, false);
+		Renderer->CreateAnimation("Player_SSGround_DiagonalDown", "CupHead_SSGround_DiagonalDown", 0.0416f, false);
 
-	Renderer->CreateAnimation("Player_Hit_Ground", "CupHead_Hit_Ground", 0.05f, false);
+		Renderer->CreateAnimation("Player_Hit_Ground", "CupHead_Hit_Ground", 0.05f, false);
 
-	Renderer->CreateAnimation("Player_Scared", "CupHead_Scared", 0.034f, false);
+		Renderer->CreateAnimation("Player_Scared", "CupHead_Scared", 0.034f, false);
+	}
+
+	{
+		Renderer->SetFrameCallback("Player_Duck", 7, [this]()
+			{
+				switch (Dir)
+				{
+				case EActorDir::Left:
+					State.ChangeState("DuckIdle_Left");
+					break;
+				case EActorDir::Right:
+					State.ChangeState("DuckIdle_Right");
+					break;
+				default:
+					break;
+				}
+			}
+		);
+	}
 }
 
 void APlayer::MakeDebugMSG()
