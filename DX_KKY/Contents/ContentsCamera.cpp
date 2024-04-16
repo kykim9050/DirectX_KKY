e7f966ba9740b5735697bf186a21ae405425b72e
kykim9050 <kykim9050@naver.com>
2024-04-16 12:21:24 +0900

    김경윤 : ContentsCamera 추가

---
+++ DX_KKY/Contents/ContentsCamera.cpp
@@ -0,0 +1,36 @@
+﻿#include "PreCompile.h"
+
+#include "ContentsCamera.h"
+
+UContentsCamera::UContentsCamera()
+{
+}
+
+UContentsCamera::~UContentsCamera()
+{
+}
+
+void UContentsCamera::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UContentsCamera::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+}
+
+void UContentsCamera::End()
+{
+	Super::End();
+}
+
+void UContentsCamera::LevelStart(ULevel* _PrevLevel)
+{
+	Super::LevelStart(_PrevLevel);
+}
+
+void UContentsCamera::LevelEnd(ULevel* _NextLevel)
+{
+	Super::LevelEnd(_NextLevel);
+}
