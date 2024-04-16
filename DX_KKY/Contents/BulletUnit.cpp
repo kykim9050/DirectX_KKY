41db12ffb7e5415a7bbf554366a3af38fbb444fb
kky <kykim9050@naver.com>
2024-04-16 17:02:57 +0900

    김경윤 : bulletUnit 클래스 추가

---
+++ DX_KKY/Contents/BulletUnit.cpp
@@ -0,0 +1,23 @@
+﻿#include "PreCompile.h"
+#include <EngineCore/DefaultSceneComponent.h>
+
+#include "BulletUnit.h"
+
+
+ABulletUnit::ABulletUnit()
+{
+}
+
+ABulletUnit::~ABulletUnit()
+{
+}
+
+void ABulletUnit::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABulletUnit::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+}
