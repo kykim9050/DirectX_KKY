796f18390ed0898ba21da422246d3c69cdf158d9
kykim9050 <kykim9050@naver.com>
2024-04-16 17:02:57 +0900

    김경윤 : bulletUnit 클래스 추가

---
+++ DX_KKY/Contents/BulletUnit.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "MoveUnit.h"
+
+// Ό³Έν :
+class ABulletUnit : public UMoveUnit
+{
+	GENERATED_BODY(UMoveUnit)
+
+public:
+	// constrcuter destructer
+	ABulletUnit();
+	~ABulletUnit();
+
+	// delete Function
+	ABulletUnit(const ABulletUnit& _Other) = delete;
+	ABulletUnit(ABulletUnit&& _Other) noexcept = delete;
+	ABulletUnit& operator=(const ABulletUnit& _Other) = delete;
+	ABulletUnit& operator=(ABulletUnit&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private:
+
+
+};
+
