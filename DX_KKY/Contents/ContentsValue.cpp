ceecc9455d9d9f98e2f3b890a205d1d5ee0e9311
kykim9050 <kykim9050@naver.com>
2024-04-17 10:55:05 +0900

    김경윤 : ContetnsValue - Bullet생성위치 변수 추가

--- DX_KKY/Contents/ContentsValue.cpp
+++ DX_KKY/Contents/ContentsValue.cpp
@@ -5,7 +5,7 @@
 const float UContentsValue::CameraInitZValue = -100.0f;
 const float UContentsValue::WorldMapPlayerXInitValue = 832.0f;
 const float UContentsValue::WorldMapPlayerYInitValue = -768.0f;
-const FVector UContentsValue::WorldMapCameraInitValue = FVector(UContentsValue::WorldMapPlayerXInitValue, 
+const float4 UContentsValue::WorldMapCameraInitValue = float4(UContentsValue::WorldMapPlayerXInitValue,
 	UContentsValue::WorldMapPlayerYInitValue, 
 	UContentsValue::CameraInitZValue);
 
@@ -14,6 +14,13 @@ const float UContentsValue::ContentsCameraInitYPos = -360.0f;
 const float4 UContentsValue::ContentsCameraInitPos = float4(UContentsValue::ContentsCameraInitXPos, UContentsValue::ContentsCameraInitYPos, UContentsValue::CameraInitZValue);
 
 const float UContentsValue::PlayerCheckPosOffSet = 32.0f;
+
+const float4 UContentsValue::Up_BulletInitPos = float4(0.0f, 100.0f, 0.0f);
+const float4 UContentsValue::DiagUp_BulletInitPos = float4(100.0f, 100.0f, 0.0f);
+const float4 UContentsValue::Straight_BulletInitPos = float4(100.0f, 0.0f, 0.0f);
+const float4 UContentsValue::DiagDown_BulletInitPos = float4(100.0f, -100.0f, 0.0f);
+const float4 UContentsValue::Down_BulletInitPos = float4(-100.0f, -100.0f, 0.0f);
+const float4 UContentsValue::Duck_BulletInitPos = float4(0.0f, -200.0f, 0.0f);
 
 
 std::shared_ptr<UEngineTexture> UContentsValue::ColMapTexture = std::shared_ptr<UEngineTexture>();
