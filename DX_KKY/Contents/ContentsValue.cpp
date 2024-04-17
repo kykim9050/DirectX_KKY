6d197dd5a8ca7a04db978848d72579b3a73b14e3
kykim9050 <kykim9050@naver.com>
2024-04-17 16:25:45 +0900

    김경윤 : Plyer - Bullet 생성 수치 변경

--- DX_KKY/Contents/ContentsValue.cpp
+++ DX_KKY/Contents/ContentsValue.cpp
@@ -15,14 +15,14 @@ const float4 UContentsValue::ContentsCameraInitPos = float4(UContentsValue::Cont
 
 const float UContentsValue::PlayerCheckPosOffSet = 32.0f;
 
-const float4 UContentsValue::Up_BulletInitPos = float4(0.0f, 100.0f, 0.0f);
-const float4 UContentsValue::RightUp_BulletInitPos = float4(50.0f, 100.0f, 0.0f);
-const float4 UContentsValue::DiagUp_BulletInitPos = float4(100.0f, 100.0f, 0.0f);
-const float4 UContentsValue::Straight_BulletInitPos = float4(100.0f, 0.0f, 0.0f);
-const float4 UContentsValue::DiagDown_BulletInitPos = float4(100.0f, -100.0f, 0.0f);
-const float4 UContentsValue::Down_BulletInitPos = float4(0.0f, -100.0f, 0.0f);
-const float4 UContentsValue::RightDown_BulletInitPos = float4(50.0f, -100.0f, 0.0f);;
-const float4 UContentsValue::Duck_BulletInitPos = float4(0.0f, -200.0f, 0.0f);
+const float4 UContentsValue::Up_BulletInitPos = float4(0.0f, 166.0f, 0.0f);
+const float4 UContentsValue::RightUp_BulletInitPos = float4(26.0f, 166.0f, 0.0f);
+const float4 UContentsValue::DiagUp_BulletInitPos = float4(64.0f, 116.0f, 0.0f);
+const float4 UContentsValue::Straight_BulletInitPos = float4(72.0f, 80.0f, 0.0f);
+const float4 UContentsValue::DiagDown_BulletInitPos = float4(64.0f, 36.0f, 0.0f);
+const float4 UContentsValue::Down_BulletInitPos = float4(0.0f, 0.0f, 0.0f);
+const float4 UContentsValue::RightDown_BulletInitPos = float4(28.0f, 0.0f, 0.0f);;
+const float4 UContentsValue::Duck_BulletInitPos = float4(80.0f, 40.0f, 0.0f);
 
 
 std::shared_ptr<UEngineTexture> UContentsValue::ColMapTexture = std::shared_ptr<UEngineTexture>();
