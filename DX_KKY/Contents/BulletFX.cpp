13d6060f2bbb5847d73911c92a44363eee9e6615
kky <kykim9050@naver.com>
2024-04-17 17:00:01 +0900

    김경윤 : BulletFX 클래스 추가 및 구현

---
+++ DX_KKY/Contents/BulletFX.cpp
@@ -0,0 +1,56 @@
+﻿#include "PreCompile.h"
+#include <EngineCore/DefaultSceneComponent.h>
+#include <EngineCore/SpriteRenderer.h>
+
+#include "BulletFX.h"
+
+ABulletFX::ABulletFX()
+{
+	UDefaultSceneComponent* Root = CreateDefaultSubObject<UDefaultSceneComponent>("FXRoot");
+	SetRoot(Root);
+
+	Renderer = CreateDefaultSubObject<USpriteRenderer>("FXRenderer");
+	Renderer->SetupAttachment(Root);
+}
+
+ABulletFX::~ABulletFX()
+{
+}
+
+void ABulletFX::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Renderer->CreateAnimation("SpawnFX", "Peashot_Spawn", 0.034f, false);
+	Renderer->SetAutoSize(1.0f, true);
+	Renderer->SetOrder(ERenderingOrder::PlayerBulletFx);
+	Renderer->SetFrameCallback("SpawnFX", 4, [this]()
+		{
+			Destroy();
+		}
+	);
+
+	StateInit();
+}
+
+void ABulletFX::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+
+	State.Update(_DeltaTime);
+}
+
+void ABulletFX::StateInit()
+{
+	State.CreateState("Spawn");
+
+	State.SetStartFunction("Spawn", [this]()
+		{
+			Renderer->ChangeAnimation("SpawnFX");
+		}
+	);
+
+	State.SetUpdateFunction("Spawn", std::bind(&ABulletFX::Spawn, this));
+
+	State.ChangeState("Spawn");
+}
