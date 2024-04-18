95ac6bc6704102df85679e9df4e1999ad2f75537
kykim9050 <kykim9050@naver.com>
2024-04-18 18:07:05 +0900

    김경윤 : BossUnit 클래스 추가

---
+++ DX_KKY/Contents/BossUnit.cpp
@@ -0,0 +1,32 @@
+﻿#include "PreCompile.h"
+#include <EngineCore/DefaultSceneComponent.h>
+
+#include "BossUnit.h"
+
+ABossUnit::ABossUnit()
+{
+	UDefaultSceneComponent* Root = CreateDefaultSubObject<UDefaultSceneComponent>("BulletRoot");
+	SetRoot(Root);
+
+	Renderer = CreateDefaultSubObject<USpriteRenderer>("Renderer");
+	Renderer->SetupAttachment(Root);
+
+	Collision = CreateDefaultSubObject<UCollision>("Collision");
+	Collision->SetupAttachment(Root);
+}
+
+ABossUnit::~ABossUnit()
+{
+}
+
+void ABossUnit::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABossUnit::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+
+	State.Update(_DeltaTime);
+}
---
+++ DX_KKY/Contents/BossUnit.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "MoveUnit.h"
+#include <EngineCore/StateManager.h>
+
+// Ό³Έν :
+class USpriteRenderer;
+class UCollision;
+class ABossUnit : public UMoveUnit
+{
+	GENERATED_BODY(UMoveUnit)
+
+public:
+	// constrcuter destructer
+	ABossUnit();
+	~ABossUnit();
+
+	// delete Function
+	ABossUnit(const ABossUnit& _Other) = delete;
+	ABossUnit(ABossUnit&& _Other) noexcept = delete;
+	ABossUnit& operator=(const ABossUnit& _Other) = delete;
+	ABossUnit& operator=(ABossUnit&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+	USpriteRenderer* Renderer;
+	UCollision* Collision;
+	UStateManager State = UStateManager();
+
+private:
+};
+
