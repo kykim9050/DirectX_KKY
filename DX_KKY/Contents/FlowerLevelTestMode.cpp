e87a2eb9fa4b36a3c92ff30d1557a5a581ac4e3d
kykim9050 <kykim9050@naver.com>
2024-04-18 16:42:52 +0900

    김경윤 : Flower Stage 이미지 리소스 추가

--- DX_KKY/Contents/FlowerLevelTestMode.cpp
+++ DX_KKY/Contents/FlowerLevelTestMode.cpp
@@ -1,6 +1,12 @@
 ﻿#include "PreCompile.h"
+#include <EngineCore/Camera.h>
 
 #include "FlowerLevelTestMode.h"
+
+
+#include "OldFilmEffect.h"
+#include "Player.h"
+#include "MapBase.h"
 
 AFlowerLevelTestMode::AFlowerLevelTestMode()
 {
@@ -13,6 +19,26 @@ AFlowerLevelTestMode::~AFlowerLevelTestMode()
 void AFlowerLevelTestMode::BeginPlay()
 {
 	Super::BeginPlay();
+
+	std::shared_ptr<UCamera> Camera = GetWorld()->SpawnActor<UCamera>("StageCamera", EActorType::ContentsCamera);
+	Camera->SetActorLocation(UContentsValue::ContentsCameraInitPos);
+
+	//std::shared_ptr<AOldFilmEffect> OldFilm = GetWorld()->SpawnActor<AOldFilmEffect>("OldFilmEffect", static_cast<int>(EActorType::FilmEffect));
+	//OldFilm->SetActorLocation(FVector{ UContentsValue::ContentsCameraInitXPos, UContentsValue::ContentsCameraInitYPos, 0.0f });
+
+	FVector WindowScale = GEngine->EngineWindow.GetWindowScale();
+
+	std::shared_ptr<AMapBase> ColMap = GetWorld()->SpawnActor<AMapBase>("ColMap", EActorType::Map);
+	ColMap->SetMapFile("Flower_Background_PixelCheck.png");
+	ColMap->SetMapScale(WindowScale);
+	ColMap->SetOrdering(ERenderingOrder::CollisionLayer);
+	ColMap->SetActorLocation(FVector(640.0f, -360.0f, 200.0f));
+	UContentsValue::ColMapTexture = UEngineTexture::FindRes(ColMap->GetName());
+	float4 ColMapScale = UContentsValue::ColMapTexture->GetScale();
+
+
+	std::shared_ptr<APlayer> Player = GetWorld()->SpawnActor<APlayer>("Player", EActorType::Player);
+	Player->SetActorLocation(FVector{ 640.0f, -360.0f, 100.0f });
 }
 
 void AFlowerLevelTestMode::Tick(float _DeltaTime)
