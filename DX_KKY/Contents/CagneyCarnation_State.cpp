1bf620b7b390cdb6a4e33d64ee5cd3a8f4bb522a
kykim9050 <kykim9050@naver.com>
2024-04-19 17:02:15 +0900

    김경윤 : FlowerBoss - 상단 얼굴 공격 상태, 애니메이션 추가

--- DX_KKY/Contents/CagneyCarnation_State.cpp
+++ DX_KKY/Contents/CagneyCarnation_State.cpp
@@ -3,31 +3,90 @@
 
 void ACagneyCarnation::StateInit()
 {
+	StateCreate();
+	StartFunctionSet();
+	UpdateFunctionSet();
+	EndFunctionSet();
+
+
+	// 
+	State.ChangeState(FlowerBossState::Intro);
+}
+
+
+void ACagneyCarnation::StateCreate()
+{
+	// Intro
+	// Idle
 	State.CreateState(FlowerBossState::Intro);
 	State.CreateState(FlowerBossState::Idle);
 
+	// FaceAttack
+	State.CreateState(FlowerBossState::FaceAttackHigh_Begin);
+	State.CreateState(FlowerBossState::FaceAttackHigh_Idle);
+	State.CreateState(FlowerBossState::FaceAttackHigh_End);
+}
 
-
+void ACagneyCarnation::StartFunctionSet()
+{
+	//Intro
 	State.SetStartFunction(FlowerBossState::Intro, [this]()
 		{
 			Renderer->ChangeAnimation(FlowerBossAniName::Flower_Intro);
 		});
+
+	//Idle
 	State.SetStartFunction(FlowerBossState::Idle, [this]()
 		{
 			Renderer->ChangeAnimation(FlowerBossAniName::Flower_Idle);
 		});
 
+	// FaceAttack
+	State.SetStartFunction(FlowerBossState::FaceAttackHigh_Begin, [this]()
+		{
+			Renderer->ChangeAnimation(FlowerBossAniName::Flower_FaceAttackHigh_Begin);
+		});
+	State.SetStartFunction(FlowerBossState::FaceAttackHigh_Idle, [this]()
+		{
+			Renderer->ChangeAnimation(FlowerBossAniName::Flower_FaceAttackHigh_Idle);
+		});
+	State.SetStartFunction(FlowerBossState::FaceAttackHigh_End, [this]()
+		{
+			Renderer->ChangeAnimation(FlowerBossAniName::Flower_FaceAttackHigh_End);
+		});
 
+}
 
+void ACagneyCarnation::UpdateFunctionSet()
+{
+	// Idle
 	State.SetUpdateFunction(FlowerBossState::Idle, [this](float)
 		{
 
 		});
+
+	//Intro
 	State.SetUpdateFunction(FlowerBossState::Intro, [this](float)
 		{
 
 		});
 
+	// FaceAttack
+	State.SetUpdateFunction(FlowerBossState::FaceAttackHigh_Begin, [this](float)
+		{
 
-	State.ChangeState(FlowerBossState::Intro);
+		});
+	State.SetUpdateFunction(FlowerBossState::FaceAttackHigh_Idle, [this](float)
+		{
+
+		});
+	State.SetUpdateFunction(FlowerBossState::FaceAttackHigh_End, [this](float)
+		{
+
+		});
+}
+
+void ACagneyCarnation::EndFunctionSet()
+{
+
 }
