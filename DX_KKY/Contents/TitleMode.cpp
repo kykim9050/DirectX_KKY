5f063518fcf4cb39acc017a25050bb8bcea384f0
kykim9050 <kykim9050@naver.com>
2024-04-19 15:24:25 +0900

    김경윤 : TitleMode 블러 적용 및 코드 수정

--- DX_KKY/Contents/TitleMode.cpp
+++ DX_KKY/Contents/TitleMode.cpp
@@ -20,13 +20,12 @@ void ATitleMode::BeginPlay()
 {
 	Super::BeginPlay();
 
-	std::shared_ptr<UCamera> Camera = GetWorld()->GetMainCamera();
-	Camera->SetActorLocation(FVector(0.0f, 0.0f, UContentsValue::CameraInitZValue));
-
-	std::shared_ptr<ATitleAnimation> TitleAni = GetWorld()->SpawnActor<ATitleAnimation>("TitleAnimation", static_cast<int>(EActorType::BackGroundAnimation));
-	std::shared_ptr<ATitleLogo> TitleLogo = GetWorld()->SpawnActor<ATitleLogo>("TitleLogo", static_cast<int>(EActorType::BackGroundSubStaticObject));
+	Camera = GetWorld()->GetMainCamera();
+	OldFilm = GetWorld()->SpawnActor<AOldFilmEffect>("TitleOldFilmEffect", static_cast<int>(EActorType::FilmEffect));
+	TitleAni = GetWorld()->SpawnActor<ATitleAnimation>("TitleAnimation", static_cast<int>(EActorType::BackGroundAnimation));
+	TitleLogo = GetWorld()->SpawnActor<ATitleLogo>("TitleLogo", static_cast<int>(EActorType::BackGroundSubStaticObject));
 	
-	std::shared_ptr<AActor> OldFilm = GetWorld()->SpawnActor<AOldFilmEffect>("TitleOldFilmEffect", static_cast<int>(EActorType::FilmEffect));
+	GetWorld()->GetLastTarget()->AddEffect<UBlurEffect>();
 }
 
 void ATitleMode::Tick(float _DeltaTime)
@@ -44,4 +43,5 @@ void ATitleMode::LevelStart(ULevel* _PrevLevel)
 {
 	Super::LevelStart(_PrevLevel);
 
+	Camera->SetActorLocation(FVector(0.0f, 0.0f, UContentsValue::CameraInitZValue));
 }
