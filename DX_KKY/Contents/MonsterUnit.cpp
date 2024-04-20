683e76ca833232305a57669982c4df9be6c71f25
kykim9050 <kykim9050@naver.com>
2024-04-20 15:40:42 +0900

    김경윤 : MonsterUnit 클래스 생성

---
+++ DX_KKY/Contents/MonsterUnit.cpp
@@ -0,0 +1,21 @@
+#include "PreCompile.h"
+
+#include "MonsterUnit.h"
+
+AMonsterUnit::AMonsterUnit()
+{
+}
+
+AMonsterUnit::~AMonsterUnit()
+{
+}
+
+void AMonsterUnit::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AMonsterUnit::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+}
---
+++ DX_KKY/Contents/MonsterUnit.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <EngineCore/Actor.h>
+
+// Ό³Έν :
+class AMonsterUnit : public AActor
+{
+	GENERATED_BODY(AActor)
+
+public:
+	// constrcuter destructer
+	AMonsterUnit();
+	~AMonsterUnit();
+
+	// delete Function
+	AMonsterUnit(const AMonsterUnit& _Other) = delete;
+	AMonsterUnit(AMonsterUnit&& _Other) noexcept = delete;
+	AMonsterUnit& operator=(const AMonsterUnit& _Other) = delete;
+	AMonsterUnit& operator=(AMonsterUnit&& _Other) noexcept = delete;
+
+	virtual void Damage(int _DmgVal) {};
+
+protected:
+	int Hp = 0;
+
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+	inline void SetHp(int _Hp)
+	{
+		Hp = _Hp;
+	}
+
+private:
+};
+
