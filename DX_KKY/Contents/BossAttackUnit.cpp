7a000f1ea9c66d02c37393d4a90e3e44f9cc9586
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-04-21 10:39:08 +0900

    김경윤 : BossAttackUnit 클래스 생성

---
+++ DX_KKY/Contents/BossAttackUnit.cpp
@@ -0,0 +1,21 @@
+#include "PreCompile.h"
+
+#include "BossAttackUnit.h"
+
+ABossAttackUnit::ABossAttackUnit()
+{
+}
+
+ABossAttackUnit::~ABossAttackUnit()
+{
+}
+
+void ABossAttackUnit::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABossAttackUnit::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+}
