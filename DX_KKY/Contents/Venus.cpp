285f96f15699cccdc3087ae58a2b2e3928b17ed9
kykim9050 <kykim9050@naver.com>
2024-04-22 14:57:51 +0900

    김경윤 : Venus 클래스 추가

---
+++ DX_KKY/Contents/Venus.cpp
@@ -0,0 +1,48 @@
+﻿#include "PreCompile.h"
+
+#include "Venus.h"
+
+AVenus::AVenus()
+{
+}
+
+AVenus::~AVenus()
+{
+}
+
+void AVenus::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AnimationInit();
+	RendererInit();
+	ColliderInit();
+	StateInit();
+}
+
+void AVenus::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+
+	State.Update(_DeltaTime);
+}
+
+void AVenus::StateInit()
+{
+
+}
+
+void AVenus::RendererInit()
+{
+
+}
+
+void AVenus::ColliderInit()
+{
+
+}
+
+void AVenus::AnimationInit()
+{
+
+}
