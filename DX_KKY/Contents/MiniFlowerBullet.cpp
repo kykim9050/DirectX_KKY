dd0194adc8eacd773866e335acd63654d9e02f06
kykim9050 <kykim9050@naver.com>
2024-04-22 22:30:07 +0900

    김경윤 : MiniFlowerBullet 클래스 생성

---
+++ DX_KKY/Contents/MiniFlowerBullet.cpp
@@ -0,0 +1,43 @@
+﻿#include "PreCompile.h"
+
+#include "MiniFlowerBullet.h"
+
+AMiniFlowerBullet::AMiniFlowerBullet()
+{
+}
+
+AMiniFlowerBullet::~AMiniFlowerBullet()
+{
+}
+
+void AMiniFlowerBullet::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StateInit();
+}
+
+void AMiniFlowerBullet::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+}
+
+void AMiniFlowerBullet::StateInit()
+{
+
+}
+
+void AMiniFlowerBullet::RendererInit()
+{
+
+}
+
+void AMiniFlowerBullet::ColliderInit()
+{
+
+}
+
+void AMiniFlowerBullet::AnimationInit()
+{
+
+}
