83e691d85cf3eed5745a83c4960dec14701c69c9
kky <kykim9050@naver.com>
2024-04-23 12:36:26 +0900

    김경윤 : Acorn 클래스 추가

---
+++ DX_KKY/Contents/Acorn.cpp
@@ -0,0 +1,48 @@
+﻿#include "PreCompile.h"
+
+#include "Acorn.h"
+
+AAcorn::AAcorn()
+{
+}
+
+AAcorn::~AAcorn()
+{
+}
+
+void AAcorn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AnimationInit();
+	RendererInit();
+	ColliderInit();
+	StateInit();
+}
+
+void AAcorn::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+
+	State.Update(_DeltaTime);
+}
+
+void AAcorn::StateInit()
+{
+
+}
+
+void AAcorn::RendererInit()
+{
+
+}
+
+void AAcorn::ColliderInit()
+{
+
+}
+
+void AAcorn::AnimationInit()
+{
+
+}
