e00a79f141733996551cf1a11beccf990c27e260
kykim9050 <kykim9050@naver.com>
2024-04-25 18:23:44 +0900

    김경윤 : Dead 상태 effect 랜덤 위치 적용 구현

--- DX_KKY/Contents/CagneyCarnation_State.cpp
+++ DX_KKY/Contents/CagneyCarnation_State.cpp
@@ -489,11 +489,12 @@ void ACagneyCarnation::Dead(float _DeltaTime)
 		ExplosionEffDelay = ExplosionEffDelayInit + ExplosionEffDelay;
 
 		float RandomAngleValue = UMath::GetInst().RandomReturnFloat(0.0f, 360.0f);
-		//float RandomXPosValue = 
+		float RandomXPosValue = UMath::GetInst().RandomReturnFloat(860.0f, 1150.0f);
+		float RandomYPosValue = UMath::GetInst().RandomReturnFloat(360.0f, 620.0f);
 
 		AFXBase* ExplosionEffect = GetWorld()->SpawnActor<AFXBase>("ExplosionEffect").get();
 		ExplosionEffect->FXInit(ERenderingOrder::FrontFX, FAniInfo(GAniName::ExplosionEffect, GSpriteName::ExplosionEffect, 0.0416f), false);
-		ExplosionEffect->SetActorLocation(GetActorLocation());
+		ExplosionEffect->SetActorLocation(float4(RandomXPosValue, -RandomYPosValue, 0.0f));
 		ExplosionEffect->SetActorRotation(float4(0.0f, 0.0f, RandomAngleValue));
 	}
 }
