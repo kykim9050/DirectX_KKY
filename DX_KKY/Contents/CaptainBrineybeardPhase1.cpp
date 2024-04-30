4f0823883ee408c05fb7860f645aec2d2368aa3e
kky <kykim9050@naver.com>
2024-04-30 10:30:29 +0900

    김경윤 : 이미지 리소스 수정

--- DX_KKY/Contents/CaptainBrineybeardPhase1.cpp
+++ DX_KKY/Contents/CaptainBrineybeardPhase1.cpp
@@ -72,7 +72,7 @@ void ACaptainBrineybeardPhase1::RendererInit()
 	ShipSailRenderer->SetPivot(EPivot::LEFTBOTTOM);
 	ShipSailRenderer->SetPosition(GRendererPos::ShipSail_RelativePos);
 
-	ShipRenderer->SetPlusColor(GColorValue::BrightColor);
+	//ShipRenderer->SetPlusColor(GColorValue::BrightColor);
 	ShipRailRenderer->SetPlusColor(GColorValue::BrightColor);
 	ShipMastRenderer->SetPlusColor(GColorValue::BrightColor);
 	ShipSailRenderer->SetPlusColor(GColorValue::BrightColor);
