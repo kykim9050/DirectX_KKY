d1115b74de6f88f973b52326e0959ab5c86f4ca1
kky <kykim9050@naver.com>
2024-05-01 16:43:31 +0900

    김경윤 : Uvula 공격용 bubble 클래스 추가

---
+++ DX_KKY/Contents/UvulaBubble.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "BulletUnit.h"
+
+// Ό³Έν :
+class AUvulaBubble : public ABulletUnit
+{
+	GENERATED_BODY(ABulletUnit)
+public:
+	// constrcuter destructer
+	AUvulaBubble();
+	~AUvulaBubble();
+
+	// delete Function
+	AUvulaBubble(const AUvulaBubble& _Other) = delete;
+	AUvulaBubble(AUvulaBubble&& _Other) noexcept = delete;
+	AUvulaBubble& operator=(const AUvulaBubble& _Other) = delete;
+	AUvulaBubble& operator=(AUvulaBubble&& _Other) noexcept = delete;
+
+protected:
+
+private:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+	void StateInit() override;
+
+	void RendererInit();
+	void ColliderInit();
+	void AnimationInit();
+};
+
