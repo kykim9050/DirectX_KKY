0473ed2eccd633bad185e7f552bbed82d85d34a2
kykim9050 <kykim9050@naver.com>
2024-05-01 16:43:31 +0900

    김경윤 : Uvula 공격용 bubble 클래스 추가

---
+++ DX_KKY/Contents/UvulaBubble.cpp
@@ -0,0 +1,41 @@
+﻿#include "PreCompile.h"
+
+#include "UvulaBubble.h"
+
+AUvulaBubble::AUvulaBubble()
+{
+}
+
+AUvulaBubble::~AUvulaBubble()
+{
+}
+
+void AUvulaBubble::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AnimationInit();
+	RendererInit();
+	ColliderInit();
+	StateInit();
+}
+
+void AUvulaBubble::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+}
+
+void AUvulaBubble::RendererInit()
+{
+
+}
+
+void AUvulaBubble::ColliderInit()
+{
+
+}
+
+void AUvulaBubble::StateInit()
+{
+
+}
