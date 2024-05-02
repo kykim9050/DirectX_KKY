ee6718e3c2fb918965013d19080faccd1d249b84
kykim9050 <kykim9050@naver.com>
2024-05-02 15:31:00 +0900

    김경윤 : Barrel 백업 애니메이션 추가

--- DX_KKY/Contents/ContentsCore.cpp
+++ DX_KKY/Contents/ContentsCore.cpp
@@ -54,6 +54,7 @@ void UContentsCore::Initialize()
 		UEngineSprite::CreateCutting("Pirate_Barrel_AttWait_Idle.png", 6, 2);
 		UEngineSprite::CreateCutting("Pirate_Barrel_Drop.png", 4, 1);
 		UEngineSprite::CreateCutting("Pirate_Barrel_Smash.png", 5, 1);
+		UEngineSprite::CreateCutting("Pirate_Barrel_BackUp.png", 6, 1);
 	}
 
 	DebugWindow = UEngineEditorGUI::CreateEditorWindow<UContentsDebugWindow>("ContentsDebugWindow");
