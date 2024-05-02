20b424fb36cc148a3672dc99c98b80da32eeb0f8
kky <kykim9050@naver.com>
2024-05-02 10:53:29 +0900

    김경윤 : Barrel 클래스 추가

---
+++ DX_KKY/Contents/Barrel.cpp
@@ -0,0 +1,57 @@
+﻿#include "PreCompile.h"
+
+#include "Barrel.h"
+
+ABarrel::ABarrel()
+{
+}
+
+ABarrel::~ABarrel()
+{
+}
+
+void ABarrel::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AnimationInit();
+	StateInit();
+}
+
+void ABarrel::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+
+	State.Update(_DeltaTime);
+}
+
+void ABarrel::RendererInit()
+{
+
+}
+
+void ABarrel::AnimationInit()
+{
+	CreateAnimation();
+	SetAnimationCallback();
+}
+
+void ABarrel::CreateAnimation()
+{
+
+}
+
+void ABarrel::ColliderInit()
+{
+
+}
+
+void ABarrel::SetAnimationCallback()
+{
+
+}
+
+void ABarrel::StateInit()
+{
+
+}
---
+++ DX_KKY/Contents/Barrel.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <EngineCore/StateManager.h>
+
+#include "MoveUnit.h"
+
+// Ό³Έν :
+class UCollision;
+class UDefaultSceneComponent;
+class USpriteRenderer;
+class ABarrel : public UMoveUnit
+{
+	GENERATED_BODY(UMoveUnit)
+public:
+	// constrcuter destructer
+	ABarrel();
+	~ABarrel();
+
+	// delete Function
+	ABarrel(const ABarrel& _Other) = delete;
+	ABarrel(ABarrel&& _Other) noexcept = delete;
+	ABarrel& operator=(const ABarrel& _Other) = delete;
+	ABarrel& operator=(ABarrel&& _Other) noexcept = delete;
+
+protected:
+
+private:
+	UStateManager State;
+
+	UDefaultSceneComponent* Root = nullptr;
+	USpriteRenderer* Renderer = nullptr;
+	UCollision* MainCollider = nullptr;
+
+
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+	void RendererInit();
+	void AnimationInit();
+	void CreateAnimation();
+	void SetAnimationCallback();
+	void ColliderInit();
+
+	void StateInit();
+};
+
