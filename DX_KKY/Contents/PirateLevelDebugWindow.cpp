c034b070c16bc96d46c5054db8ab1a65b9252ade
kky <kykim9050@naver.com>
2024-05-03 14:48:12 +0900

    김경윤 : PirateLevel 전용 디버그창 구현

---
+++ DX_KKY/Contents/PirateLevelDebugWindow.cpp
@@ -0,0 +1,30 @@
+﻿#include "PreCompile.h"
+
+#include "PirateLevelDebugWindow.h"
+
+UPirateLevelDebugWindow::UPirateLevelDebugWindow()
+{
+}
+
+UPirateLevelDebugWindow::~UPirateLevelDebugWindow()
+{
+}
+
+void UPirateLevelDebugWindow::Init()
+{
+	Super::Init();
+}
+
+void UPirateLevelDebugWindow::Tick(ULevel* Level, float _Delta)
+{
+	Super::Tick(Level, _Delta);
+}
+
+void UPirateLevelDebugWindow::OnGui(ULevel* Level, float _Delta)
+{
+	if (true == ImGui::Button("SharkAppear"))
+	{
+		int a = 0;
+		return;
+	}
+}
