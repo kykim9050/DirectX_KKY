e8f69b64d4151737af4e2623e02c153c288a8470
kykim9050 <kykim9050@naver.com>
2024-05-03 18:53:13 +0900

    김경윤 : SeaDog 애니메이션 추가

--- DX_KKY/Contents/SeaDog.cpp
+++ DX_KKY/Contents/SeaDog.cpp
@@ -22,6 +22,10 @@ void ASeaDog::BeginPlay()
 
 	AnimationInit();
 	//StateInit();
+
+	{
+		SeaDogRenderer->ChangeAnimation(PirateBossAniName::SeaDog_Appear);
+	}
 }
 
 void ASeaDog::Tick(float _DeltaTime)
@@ -33,7 +37,16 @@ void ASeaDog::Tick(float _DeltaTime)
 
 void ASeaDog::RendererInit()
 {
+	SeaDogRenderer = CreateDefaultSubObject<USpriteRenderer>("SeaDogRenderer");
+	SeaDogRenderer->SetupAttachment(Root);
+	SeaDogRenderer->SetAutoSize(1.0f, true);
+	SeaDogRenderer->SetOrder(ERenderingOrder::SubMonster1);
 
+	//EffectRenderer = CreateDefaultSubObject<USpriteRenderer>("EffectRenderer");
+	//EffectRenderer->SetupAttachment(Root);
+
+	//ScopeRenderer = CreateDefaultSubObject<USpriteRenderer>("ScopeRenderer");
+	//ScopeRenderer->SetupAttachment(Root);
 }
 
 void ASeaDog::AnimationInit()
@@ -44,7 +57,14 @@ void ASeaDog::AnimationInit()
 
 void ASeaDog::CreateAnimation()
 {
+	SeaDogRenderer->CreateAnimation(PirateBossAniName::SeaDog_Appear, "SeaDog_Appear.png", 0.0417f);
 
+	//	UEngineSprite::CreateCutting("SeaDog_Appear.png", 11, 2);
+	//UEngineSprite::CreateCutting("SeaDog_Appear_Effect.png", 6, 2);
+	//UEngineSprite::CreateCutting("SeaDog_Death.png", 8, 1);
+	//UEngineSprite::CreateCutting("SeaDog_Death_Effect.png", 5, 2);
+	//UEngineSprite::CreateCutting("SeaDog_Move.png", 6, 2);
+	//UEngineSprite::CreateCutting("SeaDog_Periscope.png", 10, 5);
 }
 
 void ASeaDog::SetAnimationCallback()
