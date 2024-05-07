e365871a30080116e6324811ba7ab5cf515d0217
kykim9050 <kykim9050@naver.com>
2024-05-07 16:03:42 +0900

    김경윤 : FlowerLevelGate 클래스 추가

---
+++ DX_KKY/Contents/FlowerLevelGate.cpp
@@ -0,0 +1,32 @@
+﻿#include "PreCompile.h"
+#include <EngineCore/DefaultSceneComponent.h>
+#include <EngineCore/SpriteRenderer.h>
+#include <EngineCore/Collision.h>
+
+#include "FlowerLevelGate.h"
+
+AFlowerLevelGate::AFlowerLevelGate()
+{
+	Root = CreateDefaultSubObject<UDefaultSceneComponent>("Root");
+	SetRoot(Root);
+
+	Renderer = CreateDefaultSubObject<USpriteRenderer>("Renderer");
+	Renderer->SetupAttachment(Root);
+
+	Collider = CreateDefaultSubObject<UCollision>("Collider");
+	Collider->SetupAttachment(Root);
+}
+
+AFlowerLevelGate::~AFlowerLevelGate()
+{
+}
+
+void AFlowerLevelGate::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AFlowerLevelGate::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+}
---
+++ DX_KKY/Contents/FlowerLevelGate.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <EngineCore/Actor.h>
+
+// Ό³Έν :
+class UDefaultSceneComponent;
+class USpriteRenderer;
+class UCollision;
+class AFlowerLevelGate : public AActor
+{
+	GENERATED_BODY(AActor)
+public:
+	// constrcuter destructer
+	AFlowerLevelGate();
+	~AFlowerLevelGate();
+
+	// delete Function
+	AFlowerLevelGate(const AFlowerLevelGate& _Other) = delete;
+	AFlowerLevelGate(AFlowerLevelGate&& _Other) noexcept = delete;
+	AFlowerLevelGate& operator=(const AFlowerLevelGate& _Other) = delete;
+	AFlowerLevelGate& operator=(AFlowerLevelGate&& _Other) noexcept = delete;
+
+protected:
+
+private:
+	UDefaultSceneComponent* Root = nullptr;
+	USpriteRenderer* Renderer = nullptr;
+	UCollision* Collider = nullptr;
+
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+};
+
