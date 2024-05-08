3d4437503b13fd0a36fa46e00a09fe9b3333dbcf
kykim9050 <kykim9050@naver.com>
2024-05-08 10:29:08 +0900

    김경윤 : titlelevel에서 anykey누르면 worldlevel로 이동하는 기능 구현

--- DX_KKY/Contents/TitleMode.h
+++ DX_KKY/Contents/TitleMode.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <EngineCore/GameMode.h>
+#include <EngineCore/StateManager.h>
 
 // 설명 :
 class UCamera;
@@ -29,17 +30,23 @@ protected:
 	void LevelStart(ULevel* _PrevLevel);
 
 private:
-	std::shared_ptr<UCamera> Camera;
-	std::shared_ptr<ATitleAnimation> TitleAni;
-	std::shared_ptr<ATitleLogo> TitleLogo;
+	std::shared_ptr<UCamera> Camera = nullptr;
+	std::shared_ptr<ATitleAnimation> TitleAni = nullptr;
+	std::shared_ptr<ATitleLogo> TitleLogo = nullptr;
 	
 	UImage* OldFilm = nullptr;
 	UImage* Iris = nullptr;
+
+	UStateManager ModeState = UStateManager();
 
 private:
 	void WidgetInit();
 	void WidgetStart();
 	void CreateObject();
 	void DeleteObject();
+
+	// State 관련
+	void StateInit();
+	void Idle(float _DeltaTime);
 };
 
