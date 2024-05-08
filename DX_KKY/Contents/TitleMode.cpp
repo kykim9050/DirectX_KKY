29b29dc42e29ca3735318a9334724e672ff9147f
kky <kykim9050@naver.com>
2024-05-08 10:18:46 +0900

    김경윤 : titleLevel 코드 보완

--- DX_KKY/Contents/TitleMode.cpp
+++ DX_KKY/Contents/TitleMode.cpp
@@ -4,7 +4,6 @@
 #include "TitleMode.h"
 #include "TitleLogo.h"
 #include "TitleAnimation.h"
-#include "OldFilmEffect.h"
 #include "ContentsEnumClass.h"
 
 ATitleMode::ATitleMode() 
@@ -21,27 +20,69 @@ void ATitleMode::BeginPlay()
 	Super::BeginPlay();
 
 	Camera = GetWorld()->GetMainCamera();
-	OldFilm = GetWorld()->SpawnActor<AOldFilmEffect>("TitleOldFilmEffect", static_cast<int>(EActorType::FilmEffect));
-	TitleAni = GetWorld()->SpawnActor<ATitleAnimation>("TitleAnimation", static_cast<int>(EActorType::BackGroundAnimation));
-	TitleLogo = GetWorld()->SpawnActor<ATitleLogo>("TitleLogo", static_cast<int>(EActorType::BackGroundSubStaticObject));
-	
+	Camera->SetActorLocation(FVector(0.0f, 0.0f, UContentsValue::CameraInitZValue));
+	WidgetInit();
 	GetWorld()->GetLastTarget()->AddEffect<UBlurEffect>();
 }
 
 void ATitleMode::Tick(float _DeltaTime)
 {
 	Super::Tick(_DeltaTime);
-
 }
 
 void ATitleMode::LevelEnd(ULevel* _NextLevel)
 {
 	Super::LevelEnd(_NextLevel);
+
+	DeleteObject();
 }
 
 void ATitleMode::LevelStart(ULevel* _PrevLevel)
 {
 	Super::LevelStart(_PrevLevel);
 
-	Camera->SetActorLocation(FVector(0.0f, 0.0f, UContentsValue::CameraInitZValue));
+	WidgetStart();
+	CreateObject();
+}
+
+void ATitleMode::WidgetInit()
+{
+	OldFilm = CreateWidget<UImage>(GetWorld(), "OldFilm");
+	OldFilm->AddToViewPort(ERenderingOrder::OldFilmEffect);
+	OldFilm->SetPosition(float4(0.0f, 0.0f, 0.0f));
+	OldFilm->SetScale(GEngine->EngineWindow.GetWindowScale());
+	OldFilm->CreateAnimation(GAniName::OldFilmAni, GSpriteName::OldFilm, 0.05f);
+
+	Iris = CreateWidget<UImage>(GetWorld(), "Iris");
+	Iris->AddToViewPort(ERenderingOrder::Iris);
+	Iris->SetPosition(float4(0.0f, 0.0f, 0.0f));
+	Iris->SetScale(GEngine->EngineWindow.GetWindowScale());
+	Iris->CreateAnimation(GAniName::IrisRevAni, GSpriteName::Iris, 0.034f, false, 16, 0);
+	Iris->SetActive(false);
+}
+
+void ATitleMode::WidgetStart()
+{
+	OldFilm->ChangeAnimation(GAniName::OldFilmAni);
+}
+
+void ATitleMode::CreateObject()
+{
+	TitleAni = GetWorld()->SpawnActor<ATitleAnimation>("TitleAnimation", static_cast<int>(EActorType::BackGroundAnimation));
+	TitleLogo = GetWorld()->SpawnActor<ATitleLogo>("TitleLogo", static_cast<int>(EActorType::BackGroundSubStaticObject));
+}
+
+void ATitleMode::DeleteObject()
+{
+	if (nullptr != TitleAni)
+	{
+		TitleAni->Destroy();
+		TitleAni = nullptr;
+	}
+
+	if (nullptr != TitleLogo)
+	{
+		TitleLogo->Destroy();
+		TitleLogo = nullptr;
+	}
 }
