82d93e46dd733edd0bfed8f1cc43e21802eff432
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-05-10 23:46:18 +0900

    김경윤 : PlayerUI 클래스 추가

---
+++ DX_KKY/Contents/PlayerUI.cpp
@@ -0,0 +1,21 @@
+#include "PreCompile.h"
+
+#include "PlayerUI.h"
+
+APlayerUI::APlayerUI()
+{
+}
+
+APlayerUI::~APlayerUI()
+{
+}
+
+void APlayerUI::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void APlayerUI::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+}
