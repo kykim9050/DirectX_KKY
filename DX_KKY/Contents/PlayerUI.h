5e89960e1260d42de4002fc52435560b53166b9c
kykim9050 <kykim9050@naver.com>
2024-05-10 23:46:18 +0900

    김경윤 : PlayerUI 클래스 추가

---
+++ DX_KKY/Contents/PlayerUI.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <EngineCore/Actor.h>
+
+// Ό³Έν :
+class UImage;
+class APlayerUI : public AActor
+{
+	GENERATED_BODY(AActor)
+
+public:
+	// constrcuter destructer
+	APlayerUI();
+	~APlayerUI();
+
+	// delete Function
+	APlayerUI(const APlayerUI& _Other) = delete;
+	APlayerUI(APlayerUI&& _Other) noexcept = delete;
+	APlayerUI& operator=(const APlayerUI& _Other) = delete;
+	APlayerUI& operator=(APlayerUI&& _Other) noexcept = delete;
+
+protected:
+
+private:
+	UImage* LifeUI = nullptr;
+
+private:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+};
+
