75443bef8f3eade73b587d241652bb6c6aec9441
kykim9050 <kykim9050@naver.com>
2024-05-11 22:55:24 +0900

    김경윤 : SuperMeter 이미지 추가

--- DX_KKY/Contents/PlayerUI.cpp
+++ DX_KKY/Contents/PlayerUI.cpp
@@ -12,6 +12,7 @@ APlayerUI::APlayerUI()
 
 APlayerUI::~APlayerUI()
 {
+
 }
 
 void APlayerUI::BeginPlay()
@@ -26,7 +27,30 @@ void APlayerUI::Tick(float _DeltaTime)
 	Super::Tick(_DeltaTime);
 }
 
+void APlayerUI::LevelEnd(ULevel* _NextLevel)
+{
+	Super::LevelEnd(_NextLevel);
+
+	SuperMeters.clear();
+}
+
 void APlayerUI::UIInit()
+{
+	LifeUIInit();
+	SuperMetersUIInit();
+}
+
+void APlayerUI::SubLife()
+{
+	if (0 == PlayerLife)
+	{
+		return;
+	}
+
+	LifeUI->ChangeAnimation(GAniName::Player_LifeUI_HP + std::to_string(--PlayerLife));
+}
+
+void APlayerUI::LifeUIInit()
 {
 	LifeUI = CreateWidget<UImage>(GetWorld(), "PlayerLifeUI");
 	LifeUI->AddToViewPort(ERenderingOrder::PlayStageUI);
@@ -38,15 +62,18 @@ void APlayerUI::UIInit()
 	LifeUI->CreateAnimation(GAniName::Player_LifeUI_HP1, "HP1");
 	LifeUI->CreateAnimation(GAniName::Player_LifeUI_HP0, "HP0");
 	LifeUI->ChangeAnimation(GAniName::Player_LifeUI_HP3);
-
 }
 
-void APlayerUI::SubLife()
+void APlayerUI::SuperMetersUIInit()
 {
-	if (0 == PlayerLife)
-	{
-		return;
-	}
+	SuperMeters.resize(SuperMeterNum);
 
-	LifeUI->ChangeAnimation(GAniName::Player_LifeUI_HP + std::to_string(--PlayerLife));
+	for (int i = 0; i < SuperMeterNum; i++)
+	{
+		SuperMeters[i] = CreateWidget<UImage>(GetWorld(), "SuperMeter"+std::to_string(i));
+		SuperMeters[i]->AddToViewPort(ERenderingOrder::PlayStageUI);
+		SuperMeters[i]->SetAutoSize(1.0f, true);
+		SuperMeters[i]->SetPosition(float4(-510.0f + 20.0f * i, -310.0f, 0.0f));
+		SuperMeters[i]->SetSprite("SuperMeterCard");
+	}
 }
